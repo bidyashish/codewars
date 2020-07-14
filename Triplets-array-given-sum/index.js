a9a668581731ddf8f4d563c25c6a6e83e98fb1ee
Bidyashish Kumar <bkumar@ultradata.com.au>
2020-07-14 11:08:15 +0800

    update

---
+++ Triplets-array-given-sum/index.js
@@ -0,0 +1,29 @@
+/* 
+Given an array and a value, find if there is a triplet in array whose sum is equal to the given value. If there is such a triplet present in array, then print the triplet. Else return false
+
+arr=[1, 4, 45, 6, 10, 8]
+
+sum=22
+
+find array[a,b,c]  where a+b+c = sum
+
+eg: From above array 4+10+8 =22
+so Answers is [4,10,8]
+
+Bidyashish Kumar
+*/
+
+function givenTriplets(arr, sum) {
+  for (let i = 0; i < arr.length - 2; i++) {
+    for (let j = i + 1; j < arr.length - 1; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] == sum) {
+          return [arr[i], arr[j], arr[k]];
+        }
+      }
+    }
+  }
+  return false;
+}
+
+console.log(givenTriplets([1, 4, 45, 6, 10, 8], 22));
